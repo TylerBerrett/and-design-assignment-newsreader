8bf8303b347b3e74f22e0e3335b3c859e3f1f152
Tyler Berrett <btylerberrett@gmail.com>
2019-08-14 17:53:13 -0600

    add images to ListPage

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.materialdesign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, ListPage::class.java)
+        button_log_in.setOnClickListener {
+            startActivity(intent)
+        }
+
     }
 }
